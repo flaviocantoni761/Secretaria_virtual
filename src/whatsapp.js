8463d6a595f8ef83a37fec521e2aabc6db43dfef
flaviocantoni761 <flaviocantoni@hotmail.com>
2026-09-09 14:47:00 -0300

    Add files via upload

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -1,4 +1,4 @@
-const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('baileys');
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
 const qrcode = require('qrcode-terminal');
 
 let sock = null;
